dd8cfedcee4112a9601bc54d45cda39c61f32656
zhangweis <cdzhangw@gmail.com>
2022-09-06 19:13:20 +0800

    Pretty print for root arrays

--- tableify.ts
+++ tableify.ts
@@ -1,3 +1,4 @@
 import tableify from "https://esm.sh/tableify";
+import forceArray from "https://esm.sh/force-array";
 import json2json from "https://raw.githubusercontent.com/zhangweis/deno-tools/main/json2json.ts";
-json2json(tableify)
+json2json((input)=>forceArray(input).map(json=>tableify(json)).join(""));
