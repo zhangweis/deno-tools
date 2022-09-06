1751244f4d9abdf614101e5d219a83aaf858253d
zhangweis <cdzhangw@gmail.com>
2022-09-06 20:12:32 +0800

    Use relative import

--- easy-table.ts
+++ easy-table.ts
@@ -1,3 +1,3 @@
 import Table from "https://esm.sh/easy-table";
-import json2json from "https://raw.githubusercontent.com/zhangweis/deno-tools/main/json2json.ts";
+import json2json from "./json2json.ts";
 json2json(Table.print)
