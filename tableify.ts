0b823a951ceda36519af27f59eb7a08703e72e5a
zhangweis <cdzhangw@gmail.com>
2022-09-06 19:44:33 +0800

    rollback tableify.ts

--- tableify.ts
+++ tableify.ts
@@ -1,4 +1,3 @@
 import tableify from "https://esm.sh/tableify";
-import forceArray from "https://esm.sh/force-array";
 import json2json from "https://raw.githubusercontent.com/zhangweis/deno-tools/main/json2json.ts";
-json2json((input)=>forceArray(input).map(json=>tableify(json)).join(""));
+json2json(tableify)
