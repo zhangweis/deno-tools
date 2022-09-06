8f294c1613602d3c1e67b4f83a6de4e38fd1e055
zhangweis <cdzhangw@gmail.com>
2022-09-06 20:12:16 +0800

    Use relative import

--- tableify.ts
+++ tableify.ts
@@ -1,3 +1,3 @@
 import tableify from "https://esm.sh/tableify";
-import json2json from "https://raw.githubusercontent.com/zhangweis/deno-tools/main/json2json.ts";
+import json2json from "./json2json.ts";
 json2json(tableify)
