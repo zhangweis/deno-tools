1da1de63f50ad337cc835c94c9c7c05d73d86949
zhangweis <cdzhangw@gmail.com>
2022-09-24 08:56:12 +0800

    Create show-qr.ts

---
+++ show-qr.ts
@@ -0,0 +1,11 @@
+import qrcode from 'https://jspm.dev/qrcode-terminal';
+import json2json from './json2json.ts';
+export default async function showQr(input:any)=>{
+    const code = await new Promise((resolve,reject)=>{
+        qrcode.generate(input.content, {small:!input.small},(code:string)=>{
+            resolve(code);
+        });
+    });
+    return Object.assign({code}, input);
+}
+if (import.meta.main) json2json(showQr);
