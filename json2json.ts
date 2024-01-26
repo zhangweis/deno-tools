b4cde791ecde2185754cb00a5117373fc041c897
John <cdzhangw@gmail.com>
2024-01-26 03:51:50 +0000

    update to avoid deprecated io

--- json2json.ts
+++ json2json.ts
@@ -1,7 +1,6 @@
 import { getStdin } from 'https://deno.land/x/get_stdin@v1.1.0/mod.ts';
 export default async function json2json(func:Function) {
-    const stdinContent = await getStdin();
-	const response = new TextDecoder().decode(stdinContent);
+    const response = await getStdin();
 	let input = JSON.parse(response);
 	let output = await func(input);  
 	if (output) console.log(JSON.stringify(output));
